319b3a69b20227b8e9d3374c3e987f8062a7b234
RyanCreates74 <uppal.ryan@gmail.com>
2026-01-09 18:51:39 +0000

    AI chatbot

--- Lesson 1/Hello.py
+++ Lesson 1/Hello.py
@@ -1,1 +1,13 @@
-print('Hello world')
+print('Hi, I am an AI chatbot, what is your name?')
+name = input()
+print('Nice to meet you', name)
+print('Are you feeling good/bad today', name, '?')
+mood = input().lower()
+if mood == 'good':
+    print("Nice to hear that! Have a great day")
+elif mood == 'bad':
+    print("Sorry to hear that, hope your day improves.")
+else:
+    print("I know it can be hard to put feelings into words!")
+
+print(f"It was nice meeting you {name}, see you soon!")
